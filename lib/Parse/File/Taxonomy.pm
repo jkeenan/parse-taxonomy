3f55fc28a5d740cc623567ee72958c7782fbfc17
James E Keenan <jkeenan@cpan.org>
2015-06-06 10:12:16 -0400

    Write POD for taxonomy-by-path vs by-index.

--- lib/Parse/File/Taxonomy.pm
+++ lib/Parse/File/Taxonomy.pm
@@ -14,23 +14,18 @@ Parse::File::Taxonomy - Validate a file for use as a taxonomy
 
     use Parse::File::Taxonomy;
 
-    $source = "./t/data/alpha.csv";
-    $obj = Parse::File::Taxonomy->new( {
-        file    => $source,
-    } );
-
-    $hashified_taxonomy = $obj->hashify_taxonomy();
-
 =head1 DESCRIPTION
 
-This module takes as input a plain-text file, verifies that it can be used as
-a taxonomy, then creates a Perl data structure representing that taxonomy.
+This module is the base class for the Parse-File-Taxonomy extension to the
+Perl 5 programming language.  You will not instantiate objects of this class;
+rather, you will instantiate objects of subclasses, of which
+Parse::File::Taxonomy::Path will be the first.
 
 B<This is an ALPHA release.>
 
 =head2 Taxonomy: definition
 
-For the purpose of this module, a B<taxonomy> is defined as a tree-like data
+For the purpose of this library, a B<taxonomy> is defined as a tree-like data
 structure with a root node, zero or more branch (child) nodes, and one or more
 leaf nodes.  The root node and each branch node must have at least one child
 node, but leaf nodes have no child nodes.  The number of branches
@@ -54,10 +49,10 @@ B<Diagram 1:>
 
 =head2 Taxonomy File:  definition
 
-For the purpose of this module, a B<taxonomy file> is (a) a CSV file in which
-one column holds data on the position of each record within the taxonomy and
-(b) in which each node in the tree other than the root node is uniquely
-represented by a record within the file.
+For the purpose of this module, a B<taxonomy file> is a CSV file in which (a)
+certain columns hold data from which the position of each record within the
+taxonomy can be derived; and (b) in which each node in the tree (other than
+the root node) is uniquely represented by a record within the file.
 
 =head3 CSV
 
@@ -65,7 +60,7 @@ B<"CSV">, strictly speaking, refers to B<comma-separated values>:
 
     path,nationality,gender,age,income,id_no
 
-For the purpose of this module, however, column separators in a taxonomy file
+For the purpose of this module, however, the column separators in a taxonomy file
 may be any user-specified character handled by the F<Text-CSV> library.
 Formats frequently observed are B<tab-separated values>:
 
@@ -93,12 +88,22 @@ data other than pure integers or floating-point numbers:
 =head3 Tree structure
 
 To qualify as a taxonomy file, it is not sufficient for a file to be in CSV
-format.  In each non-header record in that file, one column must hold data
-capable of exactly specifying the record's position in the taxonomy, I<i.e.,>
-the route or B<path> from the root node to the node being represented by that
-record.  That data must itself be in delimiter-separated format.  Each
-non-root node in the taxonomy must have exactly one record holding its path
-data.  Within that path column the value corresponding to the root node need
+format.  In each non-header record in that file, there must be one or more
+columns which hold data capable of exactly specifying the record's position in
+the taxonomy, I<i.e.,> the route or B<path> from the root node to the node
+being represented by that record.
+
+The precise way in which certain columns are used to determine the path from
+the root node to a given node is what differentiates various types of taxonomy
+files from one another.  In Parse-File-Taxonomy we identify two different
+flavors of taxonomy files and provide a class for the construction of each.
+
+=head3 Taxonomy by path
+
+A B<taxonomy by path> is one in which a single column -- which we will refer
+to as the B<path column> -- will represent the path from the root to the given
+record as a series of strings joined by separator characters.
+Within that path column the value corresponding to the root node need
 not be specified, I<i.e.,> may be represented by an empty string.
 
 Let's rewrite Diagram 1 with values to make this clear.
@@ -126,8 +131,7 @@ was a pipe (C<|>) character.  Let us further suppose that for now we are not
 concerned with the data in any columns other than C<path> so that, for purpose
 of illustration, they will hold empty (albeit quoted) string.
 
-Then the taxonomy file describing the tree in
-Diagram 2 would look like this:
+Then the taxonomy file describing the tree in Diagram 2 would look like this:
 
     "path","nationality","gender","age","income","id_no"
     "|Alpha","","","","",""
@@ -190,11 +194,72 @@ representing this tree would look like this:
     "|Gamma|Iota|Delta","","","","",""
     "|Delta","","","","",""
 
+=head3 Taxonomy by index
+
+A B<taxonomy by index> is one in which the data in which each record has a
+column with a unique identifier (B<id>) and another column holding the
+unique identifier (B<parent_id>) of the record representing the next higher node in the
+hierarchy.  The record must also a column which holds a datum that is unique
+among all records having the same parent node.
+
+Let's make this clearer by rewriting the taxonomy-by-path above for Example 3
+as a taxonomy-by-index.
+
+    "id","parent_id","name","nationality","gender","age","income","id_no"
+    1,,"Alpha","","","","",""
+    2,1,"Epsilon","","","","",""
+    3,2,"Kappa","","","","",""
+    4,1,"Zeta","","","","",""
+    5,4,"Lambda","","","","",""
+    6,4,"Mu","","","","",""
+    7,,"Beta","","","","",""
+    8,7,"Eta","","","","",""
+    9,7,"Theta","","","","",""
+    10,,"Gamma","","","","",""
+    11,10,"Iota","","","","",""
+    12,11,"Delta","","","","",""
+    13,,"Delta","","","","",""
+
+In the above taxonomy-by-index, the records with C<id>s C<1>, C<7>, C<10>, and
+C<13> are top-level nodes.   They have no parents, so the value of their
+C<parent_id> column is null or, in Perl terms, an empty string.  The records
+with C<id>s C<2> and C<4> are children of the record with C<id> of C<1>.  The
+record with C<id 3> is, in turn, a child of the record with C<id 2>.
+
+In the above taxonomy-by-index, close inspection will show that no two records
+with the same C<parent_id> share the same C<name>.  The property of
+B<uniqueness of sibling names> means that we can construct a non-indexed
+version of the path from the root to a given node by using the C<parent_id>
+column in a given record to look up the C<name> of the record with the C<id>
+value identical to the child's C<parent_id>.
+
+    Via index: 3        2       1
+
+    Via name:  Kappa    Epsilon Alpha
+
+We go from C<id 3> to its C<parent_id 2>, then to C<2>'s C<parent_id 1>.
+Putting C<name>s to this, we go from C<Kappa> to C<Epsilon> to C<Alpha>.
+
+Now, reverse the order of those C<name>s, throw a pipe delimiter before each
+of them and join them into a single string, and you get:
+
+    |Alpha|Epsilon|Kappa
+
+... which is the value of the C<path> column in the third record in the
+taxonomy-by-path displayed previously.
+
+With correct data, a given hierarchy of data can therefore be represented
+either by a taxonomy-by-path or by a taxonomy-by-index.  We would therefore
+describe these two taxonomies as B<equivalent> to each other.
+
 =head2 Taxonomy Validation
 
-The C<Parse::File::Taxonomy> constructor, C<new()>, will probe a taxonomy file
+Each C<Parse::File::Taxonomy> subclass will have a constructor, C<new()>,
+which will probe a taxonomy file
 provided to it as an argument to determine whether it can be considered a
-valid taxonomy according to the description provided above.
+valid taxonomy according to the description provided above.  The arguments
+needed for such a constructor will be found in the documentation of the
+subclass.
 
 TODO:  C<Parse::File::Taxonomy->new() should also be able to accept a
 reference to an array of CSV records already held in memory.
