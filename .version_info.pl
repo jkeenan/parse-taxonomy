4dd037c31f84fbedb8663df36cd2efde103c43ba
James E Keenan <jkeenan@cpan.org>
2015-06-21 08:00:12 -0400

    CPAN version 0.06

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.04', new => '0.05' },
-    date    => { old => 'June 17 2015', new => 'June 20 2015' },
+    version => { old => '0.05', new => '0.06' },
+    date    => { old => 'June 20 2015', new => 'June 21 2015' },
     copyright => { old => '2015', new => '2015' },
 );
 
--- lib/Parse/Taxonomy/Path.pm
+++ lib/Parse/Taxonomy/Path.pm
@@ -6,7 +6,7 @@ use Text::CSV;
 use Scalar::Util qw( reftype );
 use List::Util qw( max );
 use Cwd;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use Parse::Taxonomy::Auxiliary qw(
     path_check_fields
     components_check_fields
