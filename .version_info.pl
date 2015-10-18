0b2400e738f438534dee8bb964ca5d04ff11ebbc
James E Keenan <jkeenan@cpan.org>
2015-10-18 08:07:36 -0400

    CPAN version 0.16
    Update Changes, version numbers, dates.
    Consolidate Todo into TODO.pod.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.14', new => '0.15' },
-    date    => { old => 'October 11 2015', new => 'October 14 2015' },
+    version => { old => '0.15', new => '0.16' },
+    date    => { old => 'October 14 2015', new => 'October 18 2015' },
     copyright => { old => '2015', new => '2015' },
 );
 
--- lib/Parse/Taxonomy/MaterializedPath.pm
+++ lib/Parse/Taxonomy/MaterializedPath.pm
@@ -6,7 +6,7 @@ use Text::CSV_XS;
 use Scalar::Util qw( reftype );
 use List::Util qw( max );
 use Cwd;
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 use Parse::Taxonomy::Auxiliary qw(
     path_check_fields
     components_check_fields
