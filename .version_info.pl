7995e464870591db68cf0baa125301f086f6b7db
James E Keenan <jkeenan@cpan.org>
2015-12-21 19:08:02 -0500

    CPAN version 0.21
    Update Changes and all version numbers and dates.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.19', new => '0.20' },
-    date    => { old => 'November 12 2015', new => 'November 13 2015' },
+    version => { old => '0.20', new => '0.21' },
+    date    => { old => 'November 13 2015', new => 'December 21 2015' },
     copyright => { old => '2015', new => '2015' },
 );
 
